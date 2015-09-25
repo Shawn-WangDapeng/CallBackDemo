2a3d472d247999e54a785e13b64b4112bc50abce
peng.wang.sh <peng.wang.sh@dianping.com>
2015-09-25 17:16:15 +0800

    Initial Commit

---
+++ CallBackDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CallBackDemo
+//
+//  Created by 王鹏 on 9/25/15.
+//  Copyright © 2015 dianping.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
